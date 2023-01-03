6aae6dfb7f0378395c9a410739dd1411ac176dbc
AhmedReFu <mdnazmulkhanfeni@gmail.com>
2023-01-04 01:36:37 +0600

    first commit

---
+++ src/containers/features/Features.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './features.css';
+
+const Features = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+
+
+export default Features;
